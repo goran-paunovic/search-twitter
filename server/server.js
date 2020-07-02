229fc6e649cb72fb99a81cb90007c02c6739a911
Goran Paunović <goran.paunovic@yandex.com>
2020-07-02 18:35:14 +0200

    Added localhost:5000 to cors whitelist

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:5000"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
